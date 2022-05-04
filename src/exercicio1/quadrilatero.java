ebe0d755a28a53624ba1b70423530b6998529738
JadeRios <rios.jade@gmail.com>
2022-05-04 11:51:12 -0300

    update exercicio quadrilátero

---
+++ src/exercicio1/quadrilatero.java
@@ -0,0 +1,20 @@
+package exercicio1;
+
+public class quadrilatero {
+
+
+    public static void calcularArea(double lado) {
+        double areaQuadrado = lado * 2;
+        System.out.println("A área do quadrado é igual a " + areaQuadrado);
+    }
+
+    public static void calcularArea(double altura, double largura) {
+        double areaRetangulo = altura * largura;
+        System.out.println("A área do retangulo é igual a " + areaRetangulo);
+    }
+
+    public static void calcularArea(double base1, double base2, double altura) {
+        double areaTrapezio = ((base1 + base2) * altura)/ 2;
+        System.out.println("A área do losango é igual a " + areaTrapezio);
+    }
+}
