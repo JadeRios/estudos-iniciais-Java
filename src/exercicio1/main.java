d0c76ffdafc7bb1f9f76132cf7786a7bba6b74e2
JadeRios <rios.jade@gmail.com>
2022-05-04 11:49:01 -0300

    update exercicio quadrilátero

--- src/exercicio1/main.java
+++ src/exercicio1/main.java
@@ -5,7 +5,7 @@ public class main {
     public static void main(String[] args) {
 
 
-        System.out.println("Exercício calculadora");
+        System.out.println("Exercício Calculadora");
         calculadora.soma(3, 5);
         calculadora.subtracao(4, 5);
         calculadora.multiplicacao(2.5, 5);
@@ -21,6 +21,11 @@ public class main {
         emprestimo.calcular(1000, emprestimo.getDuasParcelas());
         emprestimo.calcular(1000, emprestimo.getTresParcelas());
         emprestimo.calcular( 1000,5);
+
+        System.out.println("Exercício área quadriláteros");
+        quadrilatero.calcularArea(5);
+        quadrilatero.calcularArea(5,7);
+        quadrilatero.calcularArea(4, 6, 5);
     }
 
 
